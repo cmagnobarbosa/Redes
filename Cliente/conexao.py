5db8237e91259c55c2abe18ca7c9d288e8c13be8
Exterminus <cmagnobarbosa@gmail.com>
2017-05-06 21:46:42 -0300

    Separação em função de conexão, envio de mensagem,leitura e encerrar a conexão

--- Cliente/conexao.py
+++ Cliente/conexao.py
@@ -12,9 +12,27 @@ class Conexao:
 
     def __init__(self):
         self.servidor_end = '127.0.0.1'     # Endereco IP do Servidor
-        self.porta = 5001                   # Porta que o Servidor esta
+        self.porta = 5001
+        self.socket = ""                           # Porta que o Servidor esta
+        #self.socket_conexao = None                #Socket de conexao.
 
+    def envia_mensagem(self, mensagem):
+        """Envia mensagem para o servidor"""
+        print mensagem
+        self.socket.send(mensagem)
+
+    def ler_socket(self):
+        """Realiza a leitura do socket e retorna os dados que foram lidos."""
+        retorno= self.socket.recv(1024)
+        if len(retorno)>0:
+            return retorno
+    def encerra_conexao(self):
+        "Encerra a conexao com servidor."
+        print "Encerrando a conexão"
+        self.socket.close()
+        exit()
     def conectar(self):
+        """Conecta e salva o socket de conexao"""
         try:
             socket_conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             destino = (self.servidor_end, self.porta)
@@ -24,10 +42,4 @@ class Conexao:
             print " Erro de conexão.. verifique se o servidor está ativo."
             print "Endereço informado ", self.servidor_end, " ", self.porta
             exit()
-
-        mensagem = "Quero Jogar"
-        socket_conexao.send(mensagem)
-        resposta = socket_conexao.recv(1024)
-        if resposta is not None:
-            print"Resposta do Servidor:", resposta
-        socket_conexao.close()
+        self.socket = socket_conexao
