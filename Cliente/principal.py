4c82ca4a170abc5f0302819bdf0d03eb09f72012
Exterminus <cmagnobarbosa@gmail.com>
2017-05-16 19:38:28 -0300

    Teste..

--- Cliente/principal.py
+++ Cliente/principal.py
@@ -31,7 +31,7 @@ class Principal(Gui):
 
     def __init__(self):
         #---HABILITAR BOTAO TRUCO---
-        #Ative para ativar a opção de pedir truco..
+        # Ative para ativar a opção de pedir truco..
         self.truco_habilitado = 1
         #--------------------
 
@@ -51,12 +51,12 @@ class Principal(Gui):
         self.flag_truco = 0
         self.gui.pontos = "0000"
         self.gui.partidas = "000"
-        self.question_truco="0"
+        self.question_truco = "0"
         self.proposta_truco_equipe = "0"
         self.resposta_proposta_truco = "0"
         self.mesa_jogo = "000000"
-        self.gui.mensagem_vez="Aguarde..."
-        self.gui.cont_cartas= 3
+        self.gui.mensagem_vez = "Aguarde..."
+        self.gui.cont_cartas = 3
         #-----------------
         self.quee = Queue()
 
@@ -65,21 +65,17 @@ class Principal(Gui):
         self.verifica.daemon = True
         self.verifica.start()
 
-
-
-
     def atualiza_mensagem(self):
         "Atualiza o campo de mensagens.."
 
         if(self.sua_vez is 0):
 
-
-            self.gui.mensagem_vez="Aguarde..."
-            self.gui.escrever(self.gui.mensagem_vez,(40,430),(255,0,0))
+            self.gui.mensagem_vez = "Aguarde..."
+            self.gui.escrever(self.gui.mensagem_vez, (40, 430), (255, 0, 0))
         if(self.sua_vez is 1):
 
-            self.gui.mensagem_vez="Sua Vez..."
-            self.gui.escrever(self.gui.mensagem_vez,(40,430),(0,255,0))
+            self.gui.mensagem_vez = "Sua Vez..."
+            self.gui.escrever(self.gui.mensagem_vez, (40, 430), (0, 255, 0))
 
     def agrupa_cartas(self, lista):
         """Agrupa as cartas recebidas do servidor"""
@@ -102,22 +98,20 @@ class Principal(Gui):
 
         """
 
-        #self.conexao.envia_mensagem("0")
+        # self.conexao.envia_mensagem("0")
         self.mensagem_servidor = self.conexao.ler_socket()
-        #print "Me", self.mensagem_servidor
+        # print "Me", self.mensagem_servidor
         #--Extrai os dados iniciais...
         self.jogador.id = self.mensagem_servidor[0:1]
         self.jogador.equipe = self.mensagem_servidor[1:2]
         self.sua_vez = int(self.mensagem_servidor[2:3])
         cartas = self.mensagem_servidor[4:10]
-        print "ID ",self.jogador.id,"Equipe ",self.jogador.equipe,"Sua Vez ",self.sua_vez
+        print "ID ", self.jogador.id, "Equipe ", self.jogador.equipe, "Sua Vez ", self.sua_vez
         self.jogador.cartas_mao = cartas
 
         cartas = self.agrupa_cartas(cartas)
         for i in cartas:
             self.gui.cartas_recebidas.append(i)
-
-
 
     def verifica_resposta_servidor(self, fila, conexao):
         """Verifica a conexao.."""
@@ -128,8 +122,8 @@ class Principal(Gui):
 
     def processa_resposta(self, lista):
         """Vai processar a mensagem recebida"""
-        self.mensagem_servidor=lista
-        if( lista is not None):
+        self.mensagem_servidor = lista
+        if(lista is not None):
             print "resposta vinda do servidor ", lista
             self.sua_vez = int(lista[2:3])
             self.atualiza_mensagem()
@@ -139,7 +133,7 @@ class Principal(Gui):
             if(cartas != "000000"):
                 # Considerando que nos decorrer das partida o servidor não envia as
                 # cartas. Redefine a mão do jogador.
-                self.gui.cartas_recebidas=[]
+                self.gui.cartas_recebidas = []
                 self.jogador.cartas_mao = cartas
                 cartas = self.agrupa_cartas(cartas)
                 for i in cartas:
@@ -147,98 +141,121 @@ class Principal(Gui):
             self.gui.pontos = lista[10:14]
             self.gui.partidas = lista[14:17]
             self.gui.valor_rodada = lista[17:19]
-            self.question_truco= lista[19:20]
+            self.question_truco = lista[19:20]
             self.proposta_truco_equipe = lista[20:21]
             self.mesa_jogo = lista[22:30]
             self.renderiza_mesa()
 
             print self.sua_vez
-            if(self.gui.cont_cartas>1):
-                self.gui.cont_cartas=self.gui.cont_cartas-1
+            if(self.gui.cont_cartas > 1):
+                self.gui.cont_cartas = self.gui.cont_cartas - 1
 
     def renderiza_mesa(self):
         """Função que renderiza_mesa"""
-        #00 00 00 00
+        # 00 00 00 00
         self.gui.caminho_cartas
         print self.mensagem_servidor
         cartas = self.agrupa_cartas(self.mesa_jogo)
-        print "Cartas Mesa ",cartas
+        print "Cartas Mesa ", cartas
         cont = -1
         for i in cartas:
-            cont=cont+1
-            #self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_1)
-            #self.gui.update_card_adversario(1,self.cont_cartas)
+            cont = cont + 1
+            # self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_1)
+            # self.gui.update_card_adversario(1,self.cont_cartas)
             if not i == "00":
-                i = self.gui.caminho_cartas+i+".png"
+                i = self.gui.caminho_cartas + i + ".png"
 
                 if(self.jogador.id == "0"):
                     if cont is 0:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.sua_pos_carta)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.sua_pos_carta)
 
                     if cont is 1:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_1)
-                        self.gui.update_card_adversario(1,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_1)
+                        self.gui.update_card_adversario(
+                            1, self.gui.cont_cartas)
                     if cont is 2:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_2)
-                        self.gui.update_card_adversario(2,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_2)
+                        self.gui.update_card_adversario(
+                            2, self.gui.cont_cartas)
                     if cont is 3:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_3)
-                        self.gui.update_card_adversario(3,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_3)
+                        self.gui.update_card_adversario(
+                            3, self.gui.cont_cartas)
 
                 elif(self.jogador.id == "1"):
                     if cont is 0:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_3)
-                        self.gui.update_card_adversario(3,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_3)
+                        self.gui.update_card_adversario(
+                            3, self.gui.cont_cartas)
                     elif cont is 1:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.sua_pos_carta)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.sua_pos_carta)
 
                     elif cont is 2:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_1)
-                        self.gui.update_card_adversario(1,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_1)
+                        self.gui.update_card_adversario(
+                            1, self.gui.cont_cartas)
 
                     elif cont is 3:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_2)
-                        self.gui.update_card_adversario(3,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_2)
+                        self.gui.update_card_adversario(
+                            3, self.gui.cont_cartas)
 
                 elif(self.jogador.id == "2"):
                     if cont is 0:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_2)
-                        self.gui.update_card_adversario(2,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_2)
+                        self.gui.update_card_adversario(
+                            2, self.gui.cont_cartas)
                     elif cont is 1:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_3)
-                        self.gui.update_card_adversario(3,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_3)
+                        self.gui.update_card_adversario(
+                            3, self.gui.cont_cartas)
                     elif cont is 2:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.sua_pos_carta)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.sua_pos_carta)
 
                     elif cont is 3:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_1)
-                        self.gui.update_card_adversario(1,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_1)
+                        self.gui.update_card_adversario(
+                            1, self.gui.cont_cartas)
 
                 elif (self.jogador.id == "3"):
                     if cont is 0:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_1)
-                        self.gui.update_card_adversario(1,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_1)
+                        self.gui.update_card_adversario(
+                            1, self.gui.cont_cartas)
                     elif cont is 1:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_2)
-                        self.gui.update_card_adversario(2,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_2)
+                        self.gui.update_card_adversario(
+                            2, self.gui.cont_cartas)
 
                     elif cont is 2:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.pos_cartas_jog_3)
-                        self.gui.update_card_adversario(3,self.gui.cont_cartas)
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.pos_cartas_jog_3)
+                        self.gui.update_card_adversario(
+                            3, self.gui.cont_cartas)
 
                     elif cont is 3:
-                        self.gui.renderiza_cartas_jogadas(i,self.gui.sua_pos_carta)
-
-
-
-
+                        self.gui.renderiza_cartas_jogadas(
+                            i, self.gui.sua_pos_carta)
 
     def finaliza_rodada(self, valor):
         """Verifica se a rodada terminou e limpa a tela"""
         if(int(self.rodada) is not valor):
             self.gui.tela_padrao(self.jogador.equipe)
             print "Limpando a rodada"
-
 
     def prepara_mensagem(self, carta_jogada):
         """Prepara uma mensagem da carta jogada para o envio"""
@@ -250,7 +267,7 @@ class Principal(Gui):
         # Acerta a equipe
         self.mensagem_servidor = self.mensagem_servidor[
             :1] + "0" + self.mensagem_servidor[2:]
-        #Limpa mao..
+        # Limpa mao..
         self.mensagem_servidor = self.mensagem_servidor[
             :4] + "000000" + self.mensagem_servidor[10:]
 
@@ -280,7 +297,7 @@ class Principal(Gui):
             print "Solicitando Truco.."
             self.mensagem_servidor = self.mensagem_servidor[
                 :19] + self.question_truco + self.mensagem_servidor[20:]
-            print "Mensagem enviada na solicitação de Truco..",self.mensagem_servidor
+            print "Mensagem enviada na solicitação de Truco..", self.mensagem_servidor
 
     def responde_truco(self):
         """Envia uma mensagem para o servidor com a resposta do truco"""
@@ -288,10 +305,8 @@ class Principal(Gui):
         self.mensagem_servidor = self.mensagem_servidor[
             :21] + self.resposta_proposta_truco + self.mensagem_servidor[22:]
 
-        print "Enviando a Seguinte resposta de Truco ",self.mensagem_servidor
+        print "Enviando a Seguinte resposta de Truco ", self.mensagem_servidor
         self.conexao.envia_mensagem(self.mensagem_servidor)
-
-
 
     def envia_carta_servidor(self, carta_jogada):
         """Dispara cartas para o servidor e altera os campos necessarios.."""
@@ -325,7 +340,8 @@ class Principal(Gui):
                 self.gui.rodadas(self.jogador.equipe)
                 self.atualiza_mensagem()
                 self.verifica_proposta_truco()
-                self.gui.desenha_botao_truco(self.gui.valor_rodada,self.proposta_truco_equipe)
+                self.gui.desenha_botao_truco(
+                    self.gui.valor_rodada, self.proposta_truco_equipe)
                 if event.type == QUIT:
                     print "Encerrando conexão...."
                     pygame.quit()
@@ -364,7 +380,8 @@ class Principal(Gui):
                         print "carta jogada", self.gui.mao[self.carta_selecionada]
                         if (self.carta_selecionada != -1):
                             # self.gui.jogar_carta(
-                            #     self.gui.mao[self.carta_selecionada], self.conexao)
+                            # self.gui.mao[self.carta_selecionada],
+                            # self.conexao)
                             self.sua_vez = 1  # Bloqueia a mão ..
                             self.envia_carta_servidor(
                                 self.gui.mao[self.carta_selecionada])
@@ -389,30 +406,32 @@ class Principal(Gui):
                     self.gui.tela.blit(fundo, [0, 0])
                     # self.gui.rodadas()
                     # self.gui.mostra_pontuacao()
-                    #self.gui.desenha_botao_truco(self.gui.valor_rodada)
+                    # self.gui.desenha_botao_truco(self.gui.valor_rodada)
 
                     self.gui.update_card_adversario(0, 3)
                     self.gui.escrever(
                         "Para selecionar cartas escolha [1,2,3]", (30, 30),
                         self.gui.branco)
                     self.gui.escrever(
-                        "Para Jogar a carta utilize seta para frente", (30, 50),
+                        "Para Jogar a carta utilize seta para frente", (
+                            30, 50),
                         self.gui.branco)
                     self.gui.escrever(
                         "Utilize as setas direcionais para ocultar", (30, 70),
-                         self.gui.branco)
+                        self.gui.branco)
                     select = 1
                 if event.type == MOUSEBUTTONDOWN and self.sua_vez == 1:
                     pos = event.pos
                     print "Posicao ", pos
                     if (pos[0] > 670 and pos[0] < 780):
                         if(pos[1] > 471 and pos[1] < 471 + 20):
-                            #self.gui.desenha_botao_truco(self.gui.valor_rodada)
+                            # self.gui.desenha_botao_truco(self.gui.valor_rodada)
                             if (self.truco_habilitado is 1):
                                 print "entrouuu"
-                                if(self.sua_vez== "1" and (self.jogador.equipe==self.proposta_truco_equipe or self.proposta_truco_equipe=="0")):
+                                print "Variaveis do truco Sua Vez ", self.sua_vez, type(self.sua_vez), "Minha equipe ", self.jogador.equipe, type(self.jogador.equipe), "Proposta truco equipe ", self.proposta_truco_equipe, type(self.proposta_truco_equipe)
+                                if(self.sua_vez == "1" and (self.jogador.equipe == self.proposta_truco_equipe or self.proposta_truco_equipe == "0")):
                                     print "pedindo truco"
-                                    self.question_truco="1"
+                                    self.question_truco = "1"
                                     self.solicita_truco()
                                     self.flag_truco = 1
                                 else:
@@ -422,7 +441,7 @@ class Principal(Gui):
                     if (pos[0] > 363 and pos[0] < 392) and self.flag_truco is 1:
                         if (pos[1] > 236 and pos[1] < 276):
                             print "Truco Aceito"
-                            self.resposta_proposta_truco= "1"
+                            self.resposta_proposta_truco = "1"
                             self.responde_truco()
                             self.gui.tela_padrao(self.jogador.equipe)
                             self.flag_truco = 0
@@ -431,7 +450,7 @@ class Principal(Gui):
                         if (pos[1] > 237 and pos[1] < 266):
                             print "Truco Não Foi aceito"
                             self.gui.tela_padrao(self.jogador.equipe)
-                            self.resposta_proposta_truco= "0"
+                            self.resposta_proposta_truco = "0"
                             se.responde_truco()
                             self.flag_truco = 0
                             # self.cartas_jogadas()
